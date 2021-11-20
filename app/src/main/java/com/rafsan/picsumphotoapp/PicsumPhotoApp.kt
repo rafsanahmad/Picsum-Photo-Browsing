1a6608edf263e5ee71e4bb7f145f0ffa499db7e5
Rafsan Ahmad <rafsanahmad111@gmail.com>
2021-11-21 00:35:25 +0600

    updated gradle dependencies, theme, colors

---
+++ app/src/main/java/com/rafsan/picsumphotoapp/PicsumPhotoApp.kt
@@ -0,0 +1,7 @@
+package com.rafsan.picsumphotoapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PicsumPhotoApp : Application()
--- app/src/main/java/com/rafsan/picsumphotoapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/rafsan/picsumphotoapp/ui/main/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.rafsan.picsumphotoapp
+package com.rafsan.picsumphotoapp.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rafsan.picsumphotoapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
