bd0260766fbd8df548a0be9fe7eb63a5791e76c8
Rafsan Ahmad <rafsanahmad111@gmail.com>
2021-11-21 19:36:34 +0600

    fab menu animation call

--- app/src/main/java/com/rafsan/picsumphotoapp/ui/detail/FullScreenActivity.kt
+++ app/src/main/java/com/rafsan/picsumphotoapp/ui/detail/FullScreenActivity.kt
@@ -2,6 +2,9 @@ package com.rafsan.picsumphotoapp.ui.detail
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.core.app.NavUtils
 import com.bumptech.glide.Glide
 import com.rafsan.picsumphotoapp.R
@@ -11,6 +14,35 @@ import com.rafsan.picsumphotoapp.databinding.ActivityDetailBinding
 
 class FullScreenActivity : BaseActivity<ActivityDetailBinding>() {
 
+    // and initializing it with animation files that we have created
+    private val rotateOpen: Animation by lazy {
+        AnimationUtils.loadAnimation(
+            this,
+            R.anim.rotate_open
+        )
+    }
+    private val rotateClose: Animation by lazy {
+        AnimationUtils.loadAnimation(
+            this,
+            R.anim.rotate_close
+        )
+    }
+    private val fromBottom: Animation by lazy {
+        AnimationUtils.loadAnimation(
+            this,
+            R.anim.from_bottom_anim
+        )
+    }
+    private val toBottom: Animation by lazy {
+        AnimationUtils.loadAnimation(
+            this,
+            R.anim.to_bottom_anim
+        )
+    }
+
+    //used to check if fab menu are opened or closed
+    private var menuClosed = false
+
     override fun onViewReady(savedInstanceState: Bundle?) {
         super.onViewReady(savedInstanceState)
         val bundle = intent.extras
@@ -19,6 +51,8 @@ class FullScreenActivity : BaseActivity<ActivityDetailBinding>() {
             setupUI(item as ImageListItem)
         }
     }
+
+    override fun setBinding(): ActivityDetailBinding = ActivityDetailBinding.inflate(layoutInflater)
 
     private fun setupUI(item: ImageListItem) {
         with(binding) {
@@ -43,10 +77,38 @@ class FullScreenActivity : BaseActivity<ActivityDetailBinding>() {
     }
 
     private fun onOptionButtonClick() {
-
+        setVisibility(menuClosed)
+        setAnimation(menuClosed)
+        menuClosed = !menuClosed;
     }
 
-    override fun setBinding(): ActivityDetailBinding = ActivityDetailBinding.inflate(layoutInflater)
+    // A Function used to set the Animation effect
+    private fun setAnimation(menuClosed: Boolean) {
+        with(binding) {
+            if (!menuClosed) {
+                fabDownload.startAnimation(fromBottom)
+                fabShare.startAnimation(fromBottom)
+                fabOptions.startAnimation(rotateOpen)
+            } else {
+                fabDownload.startAnimation(toBottom)
+                fabShare.startAnimation(toBottom)
+                fabOptions.startAnimation(rotateClose)
+            }
+        }
+    }
+
+    // used to set visibility to VISIBLE / INVISIBLE
+    private fun setVisibility(menuClosed: Boolean) {
+        with(binding) {
+            if (!menuClosed) {
+                fabDownload.visibility = View.VISIBLE
+                fabShare.visibility = View.VISIBLE
+            } else {
+                fabDownload.visibility = View.INVISIBLE
+                fabShare.visibility = View.INVISIBLE
+            }
+        }
+    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Respond to the action bar's Up/Home button
