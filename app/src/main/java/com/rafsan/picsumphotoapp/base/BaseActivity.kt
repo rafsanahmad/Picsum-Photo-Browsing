9b0249d741f5e54f8148b4c4d8a2b59c4d20afb0
Rafsan Ahmad <rafsanahmad111@gmail.com>
2021-12-13 00:15:19 +0600

    added baseViewModel, baseActivity updated

--- app/src/main/java/com/rafsan/picsumphotoapp/base/BaseActivity.kt
+++ app/src/main/java/com/rafsan/picsumphotoapp/base/BaseActivity.kt
@@ -1,27 +1,50 @@
 package com.rafsan.picsumphotoapp.base
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.annotation.CallSuper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.viewbinding.ViewBinding
+import com.rafsan.picsumphotoapp.ext.observe
 
-abstract class BaseActivity<T : ViewBinding> : AppCompatActivity() {
+abstract class BaseActivity<VB : ViewBinding, VM : BaseViewModel> : AppCompatActivity() {
 
-    protected lateinit var binding: T
+    lateinit var binding: VB
+    lateinit var viewModel: VM
+
+    abstract fun getVM(): VM
+    abstract fun bindVM(binding: VB, vm: VM)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = this.setBinding()
         setContentView(binding.root)
-
+        viewModel = getVM()
+        bindVM(binding, viewModel)
+        with(viewModel) {
+            observe(errorMessage) { msg ->
+                Toast.makeText(
+                    this@BaseActivity,
+                    msg,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
         //ready view to use
         onViewReady(savedInstanceState)
     }
 
-    abstract fun setBinding(): T
+    abstract fun setBinding(): VB
 
     @CallSuper
     protected open fun onViewReady(savedInstanceState: Bundle?) {
         // use this method in child activity
+    }
+
+    fun launchOnLifecycleScope(execute: suspend () -> Unit) {
+        lifecycleScope.launchWhenCreated {
+            execute()
+        }
     }
 }
---
+++ app/src/main/java/com/rafsan/picsumphotoapp/base/BaseViewModel.kt
@@ -0,0 +1,49 @@
+package com.rafsan.picsumphotoapp.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.rafsan.picsumphotoapp.utils.SingleLiveEvent
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+abstract class BaseViewModel : ViewModel() {
+    var progressLiveEvent = SingleLiveEvent<Boolean>()
+    var errorMessage = SingleLiveEvent<String>()
+
+    inline fun <T> launchAsync(
+        crossinline execute: suspend () -> Response<T>,
+        crossinline onSuccess: (T) -> Unit,
+        showProgress: Boolean = true
+    ) {
+        viewModelScope.launch {
+            if (showProgress)
+                progressLiveEvent.value = true
+            try {
+                val result = execute()
+                if (result.isSuccessful)
+                    onSuccess(result.body()!!)
+                else
+                    errorMessage.value = result.message()
+            } catch (ex: Exception) {
+                errorMessage.value = ex.message
+            } finally {
+                progressLiveEvent.value = false
+            }
+        }
+    }
+
+    inline fun <T> launchPagingAsync(
+        crossinline execute: suspend () -> Flow<T>,
+        crossinline onSuccess: (Flow<T>) -> Unit
+    ) {
+        viewModelScope.launch {
+            try {
+                val result = execute()
+                onSuccess(result)
+            } catch (ex: Exception) {
+                errorMessage.value = ex.message
+            }
+        }
+    }
+}
